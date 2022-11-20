e568dccb517d1102edce7f47dc9e96e52d332f2a
Henrique Rezende <henrique.64@hotmail.com>
2022-11-20 01:18:50 -0300

    feat: csv and timex libs

--- mix.exs
+++ mix.exs
@@ -43,7 +43,9 @@ defmodule CsvReport.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:csv, "~> 3.0"},
+      {:timex, "~> 3.0"}
     ]
   end
 
