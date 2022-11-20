cd91b7f3ea0a1875356ab5755e8247961696ea57
Henrique Rezende <henrique.64@hotmail.com>
2022-11-20 01:20:48 -0300

    feat: seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,26 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias CsvReport.Repo
+alias CsvReport.Accounts.Partner
+alias CsvReport.Accounts.Registration
+
+Repo.delete_all(Partner)
+Repo.delete_all(Registration)
+
+for item <- 1..30 do
+  partner =
+    Repo.insert!(%Partner{
+      name: "Partner #{item}"
+    })
+
+  for item_reg <- 1..10 do
+    Repo.insert!(%Registration{
+      name: "Partner #{item}#{item_reg}",
+      cpf: Integer.to_string(item + item_reg) |> String.pad_leading(10, "0"),
+      email: "registration#{item}#{item_reg}@email.com",
+      partner_id: partner.id
+    })
+  end
+end
